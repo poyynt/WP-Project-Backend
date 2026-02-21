047816c3b0b8c967e23ebb6e9dde7b03806859e9
Parsa Torbati <parsa.torbati@divar.ir>
2026-02-22 00:14:03 +0330

    [feat] create default roles and permissions

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+from accounts.signals import create_default_perms, create_default_roles
 
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+    def ready(self):
+        post_migrate.connect(create_default_perms, sender=self)
+        post_migrate.connect(create_default_roles, sender=self)
---
+++ accounts/serializers.py
@@ -0,0 +1,63 @@
+from django.db import transaction
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Role, Permission
+
+User = get_user_model()
+
+
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = ["id", "codename", "name"]
+
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ["id", "name"]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "email",
+            "national_id",
+            "phone",
+        ]
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            password = validated_data.pop("password")
+            user = User(**validated_data)
+            user.set_password(password)
+            user.save()
+
+            default_role = Role.objects.get(name="base")
+            user.roles.set([default_role])
+
+            return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    roles = RoleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "national_id",
+            "phone",
+            "roles",
+        ]
