d4c24e21e38e1ef029540b610b08acc030b7601d
Parsa Torbati <parsa.torbati@divar.ir>
2026-02-22 00:14:17 +0330

    [fix] add accounts models to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from accounts.models import User, Role, Permission
+
 # Register your models here.
+
+# admin.site.register(User)
+admin.site.register(Permission)
+# admin.site.register(Role)
