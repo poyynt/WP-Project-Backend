b41d7c18547d12a08c7a78375fcd3e96e56c6262
Parsa Torbati <parsa.torbati@divar.ir>
2026-02-22 04:27:19 +0330

    [feat] add photo to suspect model

--- suspects/models.py
+++ suspects/models.py
@@ -10,14 +10,15 @@ class SuspectStatus(models.TextChoices):
     NOT_GUILTY = "not_guilty", "Not Guilty"
 
 class Suspect(models.Model):
+    image = models.ImageField(upload_to="suspects/", null=True)
     case = models.ForeignKey(Case, on_delete=models.CASCADE, related_name="suspects")
     national_id = models.CharField(max_length=10)
-    full_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     status = models.CharField(max_length=50, choices=SuspectStatus.choices, default=SuspectStatus.UNDER_INTERROGATION)
-    wanted_level = models.CharField(max_length=20, default="normal")  # normal, high
 
     def __str__(self):
-        return self.full_name
+        return self.first_name + " " + self.last_name
 
 class Investigation(models.Model):
     suspect = models.ForeignKey(Suspect, on_delete=models.CASCADE, related_name="investigations")
