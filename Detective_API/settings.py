1e43c78675223c7f1d051e120f65a315daee27ac
Parsa Torbati <parsa.torbati@divar.ir>
2026-02-27 11:46:44 +0330

    [chore] dockerize

--- Detective_API/settings.py
+++ Detective_API/settings.py
@@ -85,12 +85,26 @@ WSGI_APPLICATION = 'Detective_API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+USE_POSTGRES = os.getenv('POSTGRES_DB') and os.getenv('POSTGRES_USER') and os.getenv('POSTGRES_PASSWORD')
+
+if USE_POSTGRES:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'dbname'),
+            'USER': os.getenv('POSTGRES_USER', 'user'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),
+            'HOST': os.getenv('POSTGRES_HOST', 'db'),
+            'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.13-slim AS build
+
+WORKDIR /app
+
+COPY ./requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 8000
+
+CMD ["sh", "-c", "sleep 60 && python manage.py migrate && gunicorn --bind 0.0.0.0:8000 Detective_API.wsgi:application"]
